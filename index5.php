d76b3995ea6bf5a7ccbd5fe020a89eff61878976
Christian Motika <cmot@informatik.uni-kiel.de>
2020-07-04 16:09:22 +0200

    fix

--- index5.php
+++ index5.php
@@ -4,9 +4,9 @@ error_reporting(E_ALL);
 $file = '/opt/app-root/src/data/state.txt';
 $cnt = file_get_contents($file);
 if (!$cnt) {
- 	print("error reading state file<br><br>")	
+ 	print("error reading state file<br><br>");	
 } else {
- 	print("read state file<br><br>")	
+ 	print("read state file<br><br>");	
 }
 
 ?>
