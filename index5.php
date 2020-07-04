74d5ded4c676d3b1fd9e127d2ecadd28f9507649
Christian Motika <cmot@informatik.uni-kiel.de>
2020-07-04 16:26:58 +0200

    ddddd

--- index5.php
+++ index5.php
@@ -4,11 +4,15 @@ error_reporting(E_ALL);
 $file = '/opt/app-root/src/data/state.txt';
 $cnt = file_get_contents($file);
 if (!$cnt) {
- 	print("error reading state file<br><br>");	
+ 	print('error reading state file<br><br>');	
 } else {
- 	print("read state file<br><br>");	
+ 	print('read state file<br><br>');	
 }
+$cnt = "";
+
+$a = 5;
+$a = $a + 1;
 
 ?>
 
-Hello Lufthansa (called <? print($cnt); ?> times)
+Hello Lufthansa (called <? print($a.':'.$cnt); ?> times)
